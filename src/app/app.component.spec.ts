c59eee48dd2fb970981c1abfef42facb540eb58c
Ansen123 <ansenfp@gmail.com>
2022-10-10 23:48:32 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PostApp'`, () => {
+  it(`should have as title 'postapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PostApp');
+    expect(app.title).toEqual('postapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PostApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('postapp app is running!');
   });
 });
